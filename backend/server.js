3c1cc61173148d3082a31ae930f40df00d6948be
venkatamba.Tadivaka <venniisrinivas@gmail.com>
2025-10-31 21:02:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,26 +10,26 @@ const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Connect MongoDB
+
 connectDB();
 
-// Health check route
+
 app.get('/api/health', (req, res) => res.json({ status: 'OK', timestamp: new Date() }));
 
-// Optional root route (so browser shows something)
-app.get('/', (req, res) => res.send('✅ Task Manager API is running...'));
 
-// Routes
+app.get('/', (req, res) => res.send(' Task Manager API is running...'));
+
+
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/tasks', taskRoutes);
 
-// Error Handler (must be last)
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
